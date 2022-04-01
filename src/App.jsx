7cb947e5f3132733d4e4cb2c42be2ace1bde6871
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2022-04-01 12:36:42 -0400

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ const App = () => {
   // Initialize our editionDrop contract
   const editionDrop = useEditionDrop("0x0f8482A6a7f79049a8115A9D4b400c6F891530B4");
   // Initialize our token contract
-  const token = useToken("0x7A665658444941cd96CF4de48Eab368eb32eedb5")
+  const token = useToken("0x7A665658444941cd96CF4de48Eab368eb32eedb5");
   // Initialize our vote contract
-  const vote = useVote("0x27cD9A3e16bA1468f7Bf5301486828dca6E7D5DF
+  const vote = useVote("0x27cD9A3e16bA1468f7Bf5301486828dca6E7D5DF");
 
   // State variable for us to know if user has our NFT.
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
